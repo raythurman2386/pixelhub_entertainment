2bbd8f658e4c77d840063e8c8ffe02c86fd2626c
raythurman2386 <raymondthurman5@gmail.com>
2023-08-02 14:10:50 -0500

    google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import '~/styles/global.css'
 import { IBM_Plex_Mono, Inter, PT_Serif } from '@next/font/google'
 import type { AppProps } from 'next/app'
 import { lazy } from 'react'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(process.env.NEXT_PUBLIC_ANALYTICS_GTAG);
 
 export interface SharedPageProps {
   draftMode: boolean
